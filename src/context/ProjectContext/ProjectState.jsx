4f17ee193b17d70254afe19d69261e3bfaeac034
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-09-25 19:56:39 +0200

    ProjectState completado

--- src/context/ProjectContext/ProjectState.jsx
+++ src/context/ProjectContext/ProjectState.jsx
@@ -1,7 +1,30 @@
-import React, { createContext, useReducer } from 'react';
-import axios from "axios";
+import React, { createContext, useReducer } from "react";
+import projects from "../data/projects";
 import ProjectReducer from "./ProjectReducer";
 
+
 const initialState = {
-    projects: []
-}
+  projects: [],
+  project: {},
+};
+
+export const ProductContext = createContext(initialState);
+
+export const ProjectProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(ProjectReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: "GET_PROJECTS", payload: projects });
+  }, []);
+
+  return (
+    <ProjectContext.Provider
+      value={{
+        projects: state.projects,
+        getProjects,
+      }}
+    >
+      {children}
+    </ProjectContext.Provider>
+  );
+};
