35ca1c84e82569729201c7f4413f7da8091a2493
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-09-26 18:23:36 +0200

    App estructurado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-//import {BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import ProjectCard from "./components/ProjectCard";
-import { ProjectProvider } from "./context/ProjectContext/ProjectState";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
-import Input from "./components/Input";
+//import Profile from "./pages/Profile";
+//import Projects from "./pages/Projects";
+//import Contact from "./pages/Contact";
+//import ProjectCard from "./components/ProjectCard";
+//import { ProjectProvider } from "./context/ProjectContext/ProjectState";
+//import Header from "./components/Header";
+//import Footer from "./components/Footer";
+//import Button from "./components/Button";
+//import Input from "./components/Input";
 
+function App() {
+  return (
+    <div classname="App">
+      <BrowserRouter>
+        <ProjectProvider>
+          <Header/>
+            <Routes>
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/Button" element={<Button />} />
+              <Route path="Input" element={<Input />} />
+              <Route path="ProjectCard" element={<ProjectCard />} />
+            </Routes>
+          <Footer/>
+        </ProjectProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
 
-
-
-export default App
+export default App;
--- src/context/ProjectContext/ProjectState.jsx
+++ src/context/ProjectContext/ProjectState.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer } from "react";
-import projects from "../data/projects.js";
+import projects from "../../data/projects";
 import ProjectReducer from "./ProjectReducer";
 
 
