17a4d2f39ac534df95217b2882f8f4f65dfb23b9
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-09-27 17:30:54 +0200

    estilos home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,16 @@ import foto from "../assets/foto.jpg";
 
 export default function Home() {
   return (
-    <div className="home">
-      <h1 className="title_primary">BEATRIZ CONCHADO PEIRÓ</h1>
+    <section className="home">
+      <h4 className="title_primary">BEATRIZ CONCHADO PEIRÓ</h4>
       <h3 className="text">
         Lorem ipsum dolor sit amet consectetur adipiscing elit, mi orci praesent lacus nisl lobortis, nam vivamus placerat erat sollicitudin fusce. 
       </h3>
-      <h3 className="title_secondary">
+      <h2 className="title_secondary">
         VENETATIS NOSTRA LIGULA MAFNIS FERMENTUM FAUCIBUS APTENT PALLENTESQUE
-      </h3>
-      <h5 className="title_terciary">BEATRIZ CONCHADO PEIRÓ</h5>
+      </h2>
+      <h1 className="title_terciary">BEATRIZ CONCHADO PEIRÓ</h1>
       <img src={foto} alt="foto de frente mirando a cámara" />
-    </div>
+    </section>
   );
 }
