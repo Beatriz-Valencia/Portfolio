a0de5660f306f7450f8ca7948b6f92f39c30e9b7
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-09-29 14:18:06 +0200

    media queries home en proceso

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import foto from "../assets/foto.jpg";
 export default function Home() {
   return (
     <section className="home">
-      <h4 className="title_primary">BEATRIZ CONCHADO PEIRÓ</h4>
+      {/* <h4 className="title_primary">BEATRIZ CONCHADO PEIRÓ</h4> */}
       <h3 className="text">
-        React | Node | Express | JavaScript | CSS | MongoDB | SQL Strong track record in project leadership and team management.
+        React | Node | Express | JavaScript | CSS | MongoDB | SQL Strong track record in project leadership and team management
       </h3>
       <h2 className="title_secondary">
         FULL-STACK DEVELOPER SPECIALISING IN FRONTEND AND BACKEND ARCHITECTURES
