283f7b5a289b017d299714f068dec3d38c27133a
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-10-06 13:10:15 +0200

    project cards grid

--- src/components/Projects/Project/ProjectCard.jsx
+++ src/components/Projects/Project/ProjectCard.jsx
@@ -4,8 +4,8 @@ import "../Project/ProjectCard.css";
 
 function ProjectCard ({project}) {
     return (
-        <Link //enlace SPA sin recarga a la rua del detalle
-            to={`/project/${project.id}`} //Construye la url con el id del proyecto   REVISAR
+        <Link //enlace SPA sin recarga a la ruta del detalle
+            to={`/project/${project.id}`} //Construye la url con el id del proyecto   
             className="card-link" //clase para estilos del enlace que envuelve la tarjeta
             >
                 <div className="card">
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import projects from "../data/data";
 import "./Projects.css";
 import ProjectCard from "../components/Projects/Project/ProjectCard";
@@ -8,9 +7,7 @@ export default function Projects() {
   return (
     <section className="projects-container">
       <header className="projects-header">
-        <div>
           <h1 className="projects-title">Projects</h1>
-        </div>
       </header>
       <div className="projects-grid"> {/*grid responsive para renderizado de tarjetas*/}
         {projects.map((p) => (
