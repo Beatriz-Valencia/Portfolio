1aedda0b57a2d0682839f7ef13ad4b8b963e06e4
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-10-07 12:51:54 +0200

    videos included in project files not embedded from youtube

--- src/data/data.js
+++ src/data/data.js
@@ -5,7 +5,8 @@ const project = [
         "description": "Web application in React focusing on text conversion into a spoken narration accompanied by an automatic slideshow of related images retrieved from Unsplash. It allows the user to write text, choose a visual theme, and play both synchronously.",
         "cover": "/images/narraTexto-cover.png",
         "images": "/images/narraTexto-img-1.png",
-        "youtubeUrl": "https://youtu.be/9vzpnKb9Y_8",
+        // "youtubeUrl": "https://youtu.be/9vzpnKb9Y_8",
+        "youtubeUrl": "/images/Demo-narraTexto.mp4",
         "size":"sm"
     },
 
@@ -15,7 +16,8 @@ const project = [
         "description": "React application focusing on the generation of a visual presentation from a chosen title whose objective is to track audience interactions.The images are fetched from Unsplash and displayed in an automatic slideshow. There is no backend: the data is embedded in the URL itself, and the metrics (number of views, pauses, and stops) are stored in the browser.",
          "cover": "/images/metricasAudiencia-cover.png",
         "images": "/images/metricasAudiencia-img-1.png",
-        "youtubeUrl": "https://youtu.be/MCTZkbKMdsI",
+        // "youtubeUrl": "https://youtu.be/MCTZkbKMdsI",
+        "youtubeUrl": "Demo-metricasAudiencia.mp4",
         "size": "md"
 
     },
@@ -26,7 +28,8 @@ const project = [
         "description": "Social network specially designed for pets. Each pet can have its own profile, share its thoughts in the form of posts, comments or likes on what their furry friends are doing, and build their own online community.",
          "cover": "/images/patitas-img-cover.png",
         "images": "/images/patitas-img-1.png",
-        "youtubeUrl": "https://youtu.be/IVI2gf4T08s",
+        // "youtubeUrl": "https://youtu.be/IVI2gf4T08s",
+        "youtubeUrl": "/images/Demo-Patitas Conectadas.mp4",
         "size":"sm"
 
     },
@@ -37,7 +40,8 @@ const project = [
         "description": "E-commerce platform focused on selling manga comics across different categories. The project includes a frontend application developed in React and a complete backend built with Node.js, Express, and MongoDB. The application features user registration and authentication, product management, categories and filters, shopping cart, and order handling.",
          "cover": "/images/akibaMart-img-1.png",
         "images": "/images/akibaMart-img-2.png",
-        "youtubeUrl": "https://youtu.be/7sjoNKHQ3LQ",
+        // "youtubeUrl": "https://youtu.be/7sjoNKHQ3LQ",
+        "youtubeUrl" : "/images/Demo-Akiba-Mart.mp4",
         "size":"md"
     },
 ];
