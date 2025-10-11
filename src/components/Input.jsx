4c8dc2a71b3960526f84ad1a8960de7251c9b8ea
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-10-11 14:01:12 +0200

    form input component completed

---
+++ src/components/Input.jsx
@@ -0,0 +1,34 @@
+export default function Input ({
+    name,
+    label,
+    type="text",
+    required=false,
+    value,
+    onChange,
+    autoComplete="off",
+
+}) {
+    const id =name;
+
+    return (
+        <label className ="field" htmlFor={id}>
+            <span className="field_label">
+            {label}{required && "*"}
+            </span>
+
+        <input
+        id={id}
+        name={name}
+        className="field_input"
+        type={type}
+        required={required}
+        value={value}
+        onChange={onChange}
+        autoComplete={autoComplete}
+        />
+        </label>
+
+    )
+}
+
+
---
+++ src/pages/Connect.jsx
@@ -0,0 +1,17 @@
+import "./Contact.css";
+
+
+export default function Contact() {
+    return (
+        <section className="connect">
+          <h1 className="introConnect">
+            LET'S START THE CONVERSATION
+          </h1>
+
+
+
+        </section>
+    )
+}
+
+
