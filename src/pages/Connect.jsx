92745d617b618a44bedafdfdcbf318dbc7b1ad14
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-10-11 20:02:44 +0200

    Connect screen in progress

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -1,17 +1,75 @@
-import "./Contact.css";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import Input from "../components/Input"
+import "./Connect.css";
 
+const Connect = () => {
+  const navigate = useNavigate();
 
-export default function Contact() {
-    return (
-        <section className="connect">
-          <h1 className="introConnect">
-            LET'S START THE CONVERSATION
-          </h1>
+  const [formData, setformData] = useState ({
+    name:"",
+    email:"",
+    phone:"",
+    message:""
+  });
+//Estado para guardar errores
+  const [error, setError] = useState("");
 
+  //Manejo de cambios
+  const handleChange = (e) => {
+    setformData ({
+      ...formData,
+      [e.target.name] : e.target.value, //actualizamos el campo que cambió
+    });
+  };
 
+  //Manejo del envío del formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        </section>
-    )
+  //Verificación de campos no vacíos
+if (!formData.name || !formData.email || !formData.message) {
+  setError ("Por favor, completa los campos obligatorios");
+  return
 }
+setError("");
 
+//Construimos el mailto 
+const to="beconpe.enquiries@gmail.com";
+const subject = `Nuevo mensaje de ${formData.name}`;
+const bodyLine = [
+  `Nombre: ${formData.name}`,
+  `Email: ${formData.email}`,
+  `Teléfono: ${formData.email}`,
+  "",
+  "Mensaje;", formData.message,
+]
+const body = bodyLines.join("\n");
+
+//Codificamos la URL mailto
+
+const href =`mailto:${to}?subject=${encodeURIComponent}(subject)`&body=${encondeURIComponent(body)};
+
+//Abrimos el cliente de correo con el mensaje pre-rellenado
+window.location.href= href;
+
+//Render del formulario
+return (
+  <div className="connect_background">
+    <div className="connect_container">
+      <h3>LET'S START THE CONVERSATION</h3>
+    </div>
+  </div>
+)
+
+
+  }
+
+
+
+
+
+
+
+}
 
