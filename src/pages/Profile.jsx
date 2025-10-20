3a1be3f53f83fe59f1441d1855e391ac430df1ad
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-10-20 12:17:57 +0200

    profile photo added and profile text updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import "./Profile.css";
-import SamplePicture from "../assets/SamplePicture.jpg";
+// import SamplePicture from "../assets/SamplePicture.jpg";
+import FotoPortatilColor from "../assets/Foto-portatil-B&W.jpg";
 
 export default function Profile() {
   return (
@@ -9,18 +10,19 @@ export default function Profile() {
         <p>
         I’ve spent over a decade leading design and construction projects in London — at iconic venues such as the British Library, 
      the Natural History Museum, and the Royal Opera House — and I’m now pivoting my career in Spain to focus on what I enjoy most: 
-     digital products and tech projects. A bit more about me: I studied Architecture at the Polytechnic University of Valencia and 
-     specialised in management of cultural/educational projects and historic-heritage restoration.
+     digital products and tech projects. Let me tell you a bit more about me: I studied Architecture at the Polytechnic University of Valencia and 
+     specialised in management of large-scale strategic and technical projects, working in cross-functional international teams.
      </p>
      <p>
       I’ve always loved working in teams and coordinating people. 
      My career took me to the UK for over a decade, where I became a PRINCE2 Practitioner, and more recently a Scrum Master and ITIL 4 Practitioner. 
-     I recently completed the Full-Stack Development bootcamp at The Bridge, where I learned technologies such as HTML, CSS, JavaScript, React, and Node.js, also 
-     databases like MongoDB and SQL, and tools like GitHub. I’ve discovered a genuine passion for programming and would love to put what I’ve learned into practice 
+     I recently completed the Full-Stack Development bootcamp at The Bridge, where I learned technologies such as HTML, CSS, JavaScript, React, and Node.js;
+     databases like MongoDB and SQL; and version control tools like GitHub. I’ve discovered a genuine passion for programming, and would love to put what I’ve learned into practice 
      by joining a technical team.
         </p>
       </div>
-      <img src={SamplePicture} alt="foto trabajando con portátil" />
+      {/* <img src={SamplePicture} alt="foto trabajando con portátil" /> */}
+      <img src={FotoPortatilColor} alt="foto trabajando con el portátil"/>
     </section>
   );
 }
